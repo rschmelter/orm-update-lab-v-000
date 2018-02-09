b5d6657890a619327813ffbd94cac3ee475da0da
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-09 21:39:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,7 +46,11 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
 end
