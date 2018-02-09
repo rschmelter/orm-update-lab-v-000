893ceb6ccf8180302c91809ac8b49d80992c2e3c
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-09 21:45:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,4 +66,10 @@ class Student
     end.first
   end
 
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+
+  end
+
 end
